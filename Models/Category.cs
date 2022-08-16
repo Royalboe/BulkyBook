2a585f1eb3024266ee34c12e052d3729e0a48e5b
Royalboe <auomidejohnson@gmail.com>
2022-08-16 15:31:20 +0100

    Connection to database

---
+++ Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        // To set primary key using annotations
+        [Key]
+        public int Id { get; set; }
+        // For validation
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+    }
+}
